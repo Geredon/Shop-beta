ea7e42581f993c648771ca645aee5110b6bc7b84
Geredon <walttt@yandex.ru>
2023-01-13 12:49:41 +0300

    hw.4 Реализовал получение данных с помощью Axios и добавил модальное окно при ошибке

--- src/api.js
+++ src/api.js
@@ -1,13 +1,30 @@
 import { mainUrl } from "./constants.js";
+import { startModale } from './modalWindow.js'
+import axios, { AxiosError } from 'axios';
 
-export async function  getCatalogFetch() {
-    const mainFetch = await fetch(mainUrl + '/item')
-    .then(response => response.json())
+export async function getCatalogFetch() {
+    const mainFetch = await axios.get(mainUrl + '/item')
+        .then(res => {
+            if(res.status === 200) {
+                return res.data;
+            }  else {
+                throw new Error("Произошла ошибка!")
+            }
+        })
+        .catch((err) => startModale(err));
     return mainFetch;
-};
-    
+}
+
 export async function getDetailedFetch(id) {
-    const detailedFetch = await fetch(mainUrl + '/item/' + id)
-    .then(response => response.json())
+    const detailedFetch = await axios.get(mainUrl + '/item/' + id)
+        .then(res => {
+            if(res.status === 200) {
+                return res.data;
+            }  else {
+                throw new Error("Произошла ошибка!")
+            }
+        })
+        .catch((err) => startModale(err));
     return detailedFetch;
 };
+
